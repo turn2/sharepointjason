2b5e7ab18b75cb833c1e0908529be60dc63cf75a
jason white <info@turn2.co>
2012-08-26 19:08:57 -0400

    first commit

---
+++ Backup/NBlog.Web/Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NBlog.Web.Application;
+using NBlog.Web.Application.Infrastructure;
+using NBlog.Web.Application.Service;
+using NBlog.Web.Application.Storage;
+using NBlog.Web.Application.Storage.Json;
+
+namespace NBlog.Web.Controllers
+{
+    public partial class AdminController : LayoutController
+    {
+        private readonly IRepository _repository;
+
+        public AdminController(IServices services, IRepository repositoryToBackup)
+            : base(services)
+        {
+            _repository = repositoryToBackup;
+        }
+
+        [AdminOnly]
+        [HttpGet]
+        public ActionResult Backup()
+        {
+            var jsonRepository = _repository as JsonRepository;
+            
+            if (jsonRepository == null)
+                throw new Exception("Backup currently supports only JsonRepository");
+
+            var backupFilename = Services.Cloud.ArchiveFolder(jsonRepository.DataPath);
+
+            return Content("Backup complete: " + backupFilename);
+        }
+    }
+
+}
